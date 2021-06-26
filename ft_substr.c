1370973afa5fd9f5ee07d85683941bbea1f9bcb5
PSousaF <81575097+PSousaF@users.noreply.github.com>
2021-06-26 17:55:41 -0300

    Guaca continue a nada

--- ft_substr.c
+++ ft_substr.c
@@ -5,7 +5,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	j;
 	char	*c;
+	size_t	x;
 
+	x = ft_strlen(s);
+	if (len > x)
+		len = x;
 	c = (char *)malloc(sizeof(*s) * (len + 1));
 	if (!c)
 		return (NULL);
